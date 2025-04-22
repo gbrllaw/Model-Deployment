1e8393a0686bd1a21a20ed7125e3566ce9c39510
gbrllaw <geboy2519@gmail.com>
2025-04-22 14:41:02 +0700

    coba_oop.py

--- coba_oop.py
+++ coba_oop.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
